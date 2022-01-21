4b1662b51063e50873cf1f37032e21494c901152
Jaiswar <jaiswarnilesh2002@gmail.com>
2022-01-21 11:30:21 +0530

    updated

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ['res.cloudinary.com','m.media-amazon.com','drive.google.com']
+  },
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -3,8 +3,10 @@ import Logo from '../src/images/logo.png'
 const index = () => {
   return (
     <div>
-      <Image src={Logo} width={300} height={300} />
-      <h1>Currently In Production</h1>
+      <Image src={"https://res.cloudinary.com/dykwfe4cr/image/upload/v1642744232/logo_upg6p6.png"} width={1900} height={900} />
+      <div className="title">
+      <h1>Coming Soon</h1>
+      </div>
     </div>
   );
 };
