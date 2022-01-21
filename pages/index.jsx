67d16e0407cdc0c21b47e6252252186ed13b0f02
Jaiswar <jaiswarnilesh2002@gmail.com>
2022-01-21 10:09:41 +0530

    updtaed

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,9 @@
-
-
-
+import Image from 'next/image';
+import Logo from '../src/images/logo.png'
 const index = () => {
   return (
     <div>
+      <Image src={Logo} width={300} height={300} />
       <h1>Currently In Production</h1>
     </div>
   );
