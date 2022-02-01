921c5e43ed81c49881cb6c4bf080d35857242cef
Jaiswar <jaiswarnilesh2002@gmail.com>
2022-02-01 15:51:51 +0530

    updated

--- src/components/Home/SubComponent/Influencer.jsx
+++ src/components/Home/SubComponent/Influencer.jsx
@@ -6,6 +6,7 @@ import { Pagination, Navigation,Autoplay } from "swiper";
 
 import "swiper/css/pagination";
 import "swiper/css/navigation";
+import { useState,useEffect } from "react";
 
 const Influencer = () => {
     const influencerData = [
@@ -58,17 +59,24 @@ const Influencer = () => {
             slidesPerView:4
         }
     }
+    const [show, setShow] = useState(false);
+    const [width, setWidth] = useState(0);
+    console.log({show})
+
+    useEffect(()=>{
+        setWidth(window.innerWidth)
+    },[show,width])
     return (
         <div className='redwine__home-influencer'>
             <div className="redwine__home-influencer__title">
-                <motion.h1 initial={{ x: 100, opacity: 0 }} whileInView={{ x: 0, opacity: 1 }} transition={{ duration: 1 }} viewport={{ once: true }}>Influencer</motion.h1>
+                <motion.h1 initial={{ x:  width < 700 ? 100 : 300, opacity: 0 }} whileInView={{ x: 0, opacity: 1 }} transition={{ duration: 1 }} viewport={{ once: true }}>Influencer</motion.h1>
+                <motion.span initial={{width:0,opacity:0}} whileInView={{width:width <700? 80: 130,opacity:1}} transition={{duration:1.4,delay:2,type:"spring",stiffness:300}} viewport={{once: true}}></motion.span>
             </div>
-
             <div className="redwine__home-influencer__content">
                 <Swiper
                     slidesPerView={1}
                     spaceBetween={30}
-                    autoplay={true}
+                    autoplay={{delay:3000}}
                     modules={[Pagination, Navigation,Autoplay]}
                     speed={600}
                     className="mySwiper"
@@ -76,12 +84,12 @@ const Influencer = () => {
                 >
                     {influencerData.map((d,i) => (
                         <SwiperSlide key={d.name}>
-                            <motion.div className="redwine__home-influencer__content-card" initial={{y:100,opacity:0}} whileInView={{y:0,opacity:1}} transition={{duration:1.4,delay: i<2 ? i+0.3 : 0.3}} viewport={{once: true}}>
+                            <motion.div className="redwine__home-influencer__content-card" initial={{y: width < 700 ? 100 : 300,opacity:0}} whileInView={{y:0,opacity:1}} transition={{duration:1.4,delay: width < 650 ? i<2 ? i+0.3 : 0.3 : i<4 ? i + 0.3 : 0.3 }} viewport={{once: true}} onMouseOver={()=>setShow(true)} onMouseOut={()=>setShow(false)}>
                                 <Image src={d.img} width={400} height={550} objectFit="cover" />
-                                <motion.div className="inner" whileHover={{opacity:1}} transition={{duration:1}}>
-                                    <h1>{d.name}</h1>
-                                    <p>{d.media}</p>
-                                </motion.div>
+                                <div className="inner" >
+                                    <motion.h1 initial={{translateY:100,opacity:0}} whileInView={{translateY:0, opacity:1}}>{d.name}</motion.h1>
+                                    <motion.p initial={{translateX:100,opacity:0}} whileInView={{translateX:0, opacity:1}} transition={{duration:1,delay:1,type:"spring" ,stiffness:300}}>{d.media}</motion.p>
+                                </div>
                             </motion.div>
                         </SwiperSlide>
                     ))}
